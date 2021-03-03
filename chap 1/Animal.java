f24653ce77b8c990362f28e2f373a7e32b5c0449
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-03 10:41:27 +0530

    print hello world.

--- chap 1/Animal.java
+++ chap 1/Animal.java
@@ -1,5 +1,18 @@
-class Animal{
+public class Animal{
+
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+	name = newName;
+    }
+	
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+
 }
 
-class Zoo1{
-}
