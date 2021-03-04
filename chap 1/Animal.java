b2550b4a3af12030b5174bbb013e0c4a5f0abd35
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-04 05:36:29 +0530

    Create java documentation

--- chap 1/Animal.java
+++ chap 1/Animal.java
@@ -1,17 +1,31 @@
+/**
+* This is a simple Java class with set and get methods
+* @version 1.0
+* @author  Eranda Upeshitha
+* @since 2021-03-04
+*/
 public class Animal{
 
-    String name;
+    private String name;
 
+	/**
+	* get the name of animal
+	* @return the name of the animal
+	*/
     public String getName(){
         return name;
     }
 
+	/**
+	* set the name of animal
+	* @param the name of the animal
+	*/
     public void setName(String newName){
 	name = newName;
     }
 	
-	public static void main(String args[]){
-		System.out.println("Hello World");
+	public static void main(String[] args){
+		System.out.println("Welcome!");
 	}
 
 }
