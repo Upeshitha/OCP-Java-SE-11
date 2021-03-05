45ca6f5a6791a600f42ba56e962f802d4e0d8036
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-05 08:16:08 +0530

    run java packages

---
+++ Chap 1 more depth/2 Working with packages/packageA/ClassA.java
@@ -0,0 +1,8 @@
+/**
+* This is a simple Java class to learn how to compile and run code with packages
+* @version 1.0
+* @author  Eranda Upeshitha
+* @since 2021-03-05
+*/
+package packageA;
+public class ClassA{}
---
+++ Chap 1 more depth/2 Working with packages/packageB/ClassB.java
@@ -0,0 +1,19 @@
+/**
+* This is a simple Java class to learn how to compile and run code with packages
+* @version 1.0
+* @author  Eranda Upeshitha
+* @since 2021-03-05
+*/
+package packageB;
+
+import packageA.ClassA;
+
+public class ClassB{
+	
+	public static void main(String[] args){
+		
+		ClassA a;
+		System.out.println("got it");
+	}
+
+}
