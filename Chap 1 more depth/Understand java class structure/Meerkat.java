ae940d2f75c0e2494e9cfe4bf479fd622b44c8da
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-05 07:32:40 +0530

    check 2 public classes in one file

---
+++ Chap 1 more depth/Understand java class structure/Meerkat.java
@@ -0,0 +1,16 @@
+/**
+* This is a simple Java class to test how to work java class
+* with two public classes.
+* @version 1.0
+* @author  Eranda Upeshitha
+* @since 2021-03-05
+*/
+public class Meerkat{}
+
+public class Paw{}
+
+/*
+Result - This file doesn't work correctly. Because Java not support
+         two embeded public classes.
+		 There isn't more than one public class in a file, it is okay.
+"/
